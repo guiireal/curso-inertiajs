6d823cdae48ab0aa106a7666836fabd0adc54a04
guiireal <guii_real@hotmail.com>
2024-02-15 12:26:12 -0300

    wip

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,28 +1,14 @@
 <script setup>
-import { reactive, ref } from "vue";
-import { router } from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
 
-const user = reactive({
+const form = useForm({
   name: "",
   email: "",
   password: "",
 });
 
-let isLoading = ref(false);
-
-defineProps({
-  errors: Object,
-});
-
 function submit() {
-  router.post("/users", user, {
-    onStart() {
-      isLoading.value = true;
-    },
-    onFinish() {
-      isLoading.value = false;
-    },
-  });
+  form.post("/users");
 }
 </script>
 
@@ -35,23 +21,23 @@ function submit() {
         <form @submit.prevent="submit">
           <div class="mb-3">
             <label for="name" class="form-label">Nome</label>
-            <input id="name" type="text" class="form-control" v-model="user.name">
-            <div v-if="errors.name" class="text-danger form-text">{{ errors.name }}</div>
+            <input id="name" type="text" class="form-control" v-model="form.name">
+            <div v-if="form.errors.name" class="text-danger form-text">{{ form.errors.name }}</div>
           </div>
           <div class="mb-3">
             <label for="email" class="form-label">E-mail</label>
-            <input id="email" type="email" class="form-control" v-model="user.email">
-            <div v-if="errors.email" class="text-danger form-text">{{ errors.email }}</div>
+            <input id="email" type="email" class="form-control" v-model="form.email">
+            <div v-if="form.errors.email" class="text-danger form-text">{{ form.errors.email }}</div>
           </div>
           <div class="mb-3">
             <label for="password" class="form-label">Senha</label>
-            <input id="password" type="password" class="form-control" v-model="user.password">
-            <div v-if="errors.password" class="text-danger form-text">
-              {{ errors.password }}
+            <input id="password" type="password" class="form-control" v-model="form.password">
+            <div v-if="form.errors.password" class="text-danger form-text">
+              {{ form.errors.password }}
             </div>
           </div>
-          <button type="submit" class="btn btn-primary" :disabled="isLoading">
-            <span v-if="isLoading">Enviando...</span>
+          <button type="submit" class="btn btn-primary" :disabled="form.processing">
+            <span v-if="form.processing">Enviando...</span>
             <span v-else>Enviar</span>
           </button>
         </form>
