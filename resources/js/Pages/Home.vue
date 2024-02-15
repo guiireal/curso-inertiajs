5e83b16e6979071acb76c15fc921e394847ea5a7
guiireal <guii_real@hotmail.com>
2024-02-15 01:52:19 -0300

    wip

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,11 @@
 <script setup>
-import { usePage } from "@inertiajs/vue3";
 
-const page = usePage();
-
-const { auth } = page.props;
 </script>
 
 <template>
   <AppHeader title="Home"/>
   <div>
-    <h1 v-if="$page.props?.auth?.user">Bem vindo, {{ auth.user.name }}</h1>
+    <h1 v-if="$page.props?.auth?.user">Bem vindo, {{ $page.props.auth.user.name }}</h1>
     <h1 v-else>Home</h1>
   </div>
 </template>
